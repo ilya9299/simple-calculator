4a6f9cca2a75673788e08309a75c5b5b10621005
ilya9299 <netizen9299@gmail.com>
2025-07-18 14:08:21 +0330

    Add files via upload
    calculator

---
+++ calculator.py
@@ -0,0 +1,19 @@
+
+
+numone = int(input("enter a number:"))
+
+mult = input("enter a math:")
+
+numtwo = int(input("enter a number again:"))
+
+# how_to_use1 = "+ -----> +"
+# how_to_use2 = "* -----> x"
+# how_to_use3 = "** -----> ^"
+# how_to_use4 = "/ -----> /"
+# how_to_use5 = "- -----> -"
+
+if mult == "+":print(numone + numtwo)
+if mult == "*":print (numone * numtwo)
+if mult == "^":print(numone ** numtwo)
+if mult == "-":print(numone - numtwo)
+if mult == "/":print(numone / numtwo)
